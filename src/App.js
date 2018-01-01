c5f27dfbdb9a694099c4985aaa81a2aaeafdd9be
camdenlink <camdenlink@live.com>
2018-01-01 16:19:19 -0500

    making network call to scraper. added axios/recharts dependency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import HeaderBar from './Components/HeaderBar';
 import SearchBar from './Components/SearchBar';
+import Axios from 'axios';
+import ChartComponent from './Components/ChartComponent';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
@@ -9,8 +11,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: []
+      players: [],
     }
+  }
+  /*
+  * Retrieves the per game data for given player
+  * Currently hardcoded to LeBron on Server Side
+  */
+  getSinglePlayerData(playerName) {
+      var ppgData = {};
+      var apgData = {};
+      var rpgData = {};
+      Axios.get('http://localhost:8081/per_game')
+        .then(function(response) {
+          console.log(response.data);
+          window.data = response.data;
+          for (var key in response.data.seasons) {
+
+            ppgData[key] = response.data.seasons[key]['pts_per_g'];
+            apgData[key] = response.data.seasons[key]['ast_per_g'];
+            rpgData[key] = response.data.seasons[key]['trb_per_g'];
+          	// for (var stat in response.data.seasons[key]) {
+          	// 	console.log(key + " " + stat);
+            // }
+          }
+
+          console.log(ppgData);
+          console.log(apgData);
+          console.log(rpgData);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+  }
+
+  parseData(data) {
+
   }
 
   handleAddPlayer(player) {
@@ -20,6 +56,7 @@ class App extends Component {
     this.setState({
       players: players
     });
+    this.state.players.map(player => this.getSinglePlayerData(player.name));
   }
 
   render() {
@@ -27,6 +64,7 @@ class App extends Component {
       <MuiThemeProvider>
         <HeaderBar />
         <SearchBar addPlayer={this.handleAddPlayer.bind(this)}/>
+        <ChartComponent />
       </MuiThemeProvider>
     );
   }
---
+++ src/Components/ChartComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChartComponent extends Component {
+  render() {
+    return (
+        <div className="chartArea">
+          <h1>SPACE FOR CHART</h1>
+        </div>
+    );
+  }
+}
+
+export default ChartComponent;
